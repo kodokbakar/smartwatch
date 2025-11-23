a3298d3ba2f78f1955db1b99447032e3a1edb0a7
Mochamad Amirul Ikhsani <amirulikhsani41@gmail.com>
2025-11-23 22:12:15 +0700

    feat: add login and registration routes with corresponding bindings and views

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,8 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const SPLASHSCREEN = _Paths.SPLASHSCREEN;
   static const WELCOME = _Paths.WELCOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
 }
 
 abstract class _Paths {
@@ -13,4 +15,6 @@ abstract class _Paths {
   static const HOME = '/home';
   static const SPLASHSCREEN = '/splashscreen';
   static const WELCOME = '/welcome';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
 }
