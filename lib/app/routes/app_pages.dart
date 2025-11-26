75d2cd17a2d8eef1f91bb453a24f9cfa3a471df1
RayanggaFA <rayangga09@gmail.com>
2025-11-26 14:57:47 +0700

    Update UI HMW 2 Page2View, Page2Controller, Page2Binding and routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -46,5 +46,10 @@ class AppPages {
       page: () => const RegisterView(),
       binding: RegisterBinding(),
     ),
+    GetPage(
+      name: Routes.PAGE2,
+      page: () => const Page2View(),
+      binding: Page2Binding(),
+    ),
   ];
 }
